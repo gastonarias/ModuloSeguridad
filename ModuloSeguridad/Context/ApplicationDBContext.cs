7f8e10650a27c567fe0ce48dd443664652145ece
Gaston Ariel Arias <gastonarielarias@gmail.com>
2020-10-19 23:29:47 -0300

    Creando proyecto de seguridad

---
+++ ModuloSeguridad/Context/ApplicationDBContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using ModuloSeguridad.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ModuloSeguridad.Context
+{
+    public class ApplicationDBContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options)
+            : base(options)
+        {
+
+        }
+    }
+}
